6d08f5f48bcfbea4752affc57b659dda4788a557
Marina Ivanova <marina.ivanova.ca@gmail.com>
2024-02-26 16:21:12 -0800

    Loaded Bootstrap and Created React Components

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from "react";
 import { createRoot } from "react-dom/client"; // Import createRoot from react-dom/client
+import "jquery";
+import "popper.js/dist/umd/popper";
+import "bootstrap/dist/js/bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
+import { NavBar } from "./App";
+    
 
-const element = <div>Hello World</div>;
 
 // Use createRoot to render your element
 const root = createRoot(document.getElementById("root"));
-root.render(element);
+root.render(<NavBar />);
 
-console.log(element);
+
