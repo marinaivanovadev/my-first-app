a93b2a1e40854b4dd279ffd8d265b7dde62183f6
Marina Ivanova <marina.ivanova.ca@gmail.com>
2024-03-06 22:16:02 -0800

    Render Methods

--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -16,7 +16,7 @@ export default class MainContent extends Component{
     render() {
         return (
             <div>
-                <h4 className="border-bottom m-1 p-1">
+                <h4 className="m-1 p-1">
                     {this.state.pageTitle}
                     <span className="badge badge-secondary m-2">
                         {this.state.customersCount}
@@ -33,21 +33,7 @@ export default class MainContent extends Component{
                         </tr>
                     </thead>
                     <tbody>
-                        {
-                            this.state.customers.map((cust) => {
-                                return (
-                                    <tr key={cust.id}>
-                                        <td>{cust.id}</td>
-                                        <td>{cust.name}</td>
-                                        <td>{cust.phone ?
-                                            cust.phone :
-                                            <div className="bg-warning p-2 text-center">No Phone </div>
-                                        }</td>
-                                        <td>{cust.address.city}</td>
-                                    </tr>
-                                );
-                            })
-                        }
+                        {this.getCustomerRow()}
                     </tbody>
                 </table>
             </div>
@@ -60,5 +46,26 @@ export default class MainContent extends Component{
         this.setState({
             customersCount: 7
         });
-}
+    }
+    getPhoneToRender = (phone) => {
+        if (phone)
+        return phone;
+        else {
+            return <div className="bg-warning p-2 text-center">No Phone </div>;
+        }
+    }
+
+    getCustomerRow = () => {
+        
+        return this.state.customers.map((cust) => {
+            return (
+                <tr key={cust.id}>
+                    <td>{cust.id}</td>
+                    <td>{cust.name}</td>
+                    <td>{this.getPhoneToRender(cust.phone)}</td>
+                    <td>{cust.address.city}</td>
+                </tr>
+            );
+        });
+    };
 }
