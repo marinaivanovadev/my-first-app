89834180aba51248de1b5b05e96683c4c716be84
Marina Ivanova <marina.ivanova.ca@gmail.com>
2024-03-13 14:53:32 -0700

    Pass Event Arguments

--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -16,13 +16,6 @@ export default class MainContent extends Component {
             ],
     };
 
-    customerNameStyle = (custName) => {
-        if (custName.startsWith("S")) return "pink-highlight border-left";
-        else if (custName.startsWith("R")) return "green-highlight border-right";
-        else return "";
-    };
-
-
     render() {
         return (
             <div>
@@ -68,16 +61,40 @@ export default class MainContent extends Component {
 
     getCustomerRow = () => {
 
-        return this.state.customers.map((cust) => {
+        return this.state.customers.map((cust, index) => {
             return (
                 <tr key={cust.id}>
                     <td>{cust.id}</td>
-                    <td><img src={cust.photo} alt="Customer" /></td>
-                    <td className={this.customerNameStyle(cust.name)}>{cust.name}</td>
+                    <td>
+                        <img src={cust.photo} alt="Customer" />
+                        <div>
+                            <button className="btn btn-sm btn-secondary" onClick={() => { this.onChangePictureClick(cust, index); }}>Change Picture
+                            </button>
+                        </div>                    
+                    </td>
+                    <td >{cust.name}</td>
                     <td>{this.getPhoneToRender(cust.phone)}</td>
                     <td>{cust.address.city}</td>
                 </tr>
             );
         });
+
+        
     };
+    // Executes when the user clicks on "Change Picture" button in the grid
+    // Receives the "customer" object and index of the currently clicked customer
+onChangePictureClick = (cust, index) => {
+    // console.log(cust)
+    // console.log(index);
+
+    // get existing customers
+    var custArr = this.state.customers;
+    custArr[index].photo = "https://picsum.photos/id/1064/60";
+
+
+    // update "customers" array in the state
+    this.setState({ customers: custArr });
+
+};
+
 }
