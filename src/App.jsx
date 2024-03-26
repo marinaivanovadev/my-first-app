753b54463673c35cc11262355e242e6ee665f3d9
Marina Ivanova <marina.ivanova.ca@gmail.com>
2024-03-25 20:15:22 -0700

    Basic Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
 import React, { Component } from "react";
 import NavBar from "./NavBar";
+import Dashboard from "./Dashboard";
 import Login from "./Login";
+import ShoppingCart from "./ShoppingCart";
+import CustomerList from "./CustomerList";
 
+import { BrowserRouter, Routes, Route } from "react-router-dom"; // Import Route from react-router-dom
 
-export default class App extends
-Component {
+export default class App extends Component {
     render() {
         return (
-            <React.Fragment>
+            <BrowserRouter>
                 <NavBar />
-                <Login />
-            </React.Fragment>
+                <Routes>
+                    <Route path="/" element={<Login />} />
+                    <Route path="/dashboard" element={<Dashboard />} />
+                    <Route path="/customers" element={<CustomerList />} />
+                    <Route path="/cart" element={<ShoppingCart />} />
+                </Routes>
+            </BrowserRouter>
         );
     }
-
 }
